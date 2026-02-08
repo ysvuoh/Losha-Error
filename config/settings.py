8de934203ca9cda2c69052446301dc187dc830d3
ysvuoh <ali.dhman.201@gmail.com>
2026-02-09 00:35:47 +0200

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,18 +1,25 @@
-# ===== ADMINS =====
+# ================= ADMINS =================
 ADMINS = [6196298047, 7482679982]
 
-# ===== OWNER / BRANDING =====
+# ================= OWNER / BRANDING =================
 OWNER_ID = 6196298047
 OWNER_NAME = "𝐋𝐎𝐒𝐇𝐀"
 CHANNEL_USERNAME = "L_O_S_H_A_1"
-HIT_CHAT =-1002465727289
-# ===== HTML PARTS =====
+HIT_CHAT = -1002465727289
+ADMIN_GROUP = -1002448374644
+
+# ================= HTML PARTS =================
+# رابط القناة
 CHANNEL_URL = f"https://t.me/{CHANNEL_USERNAME}"
 CHANNEL_ICON = f'<a href="{CHANNEL_URL}">ϟ</a>'
-ADMIN_GROUP = -1002448374644
-# رابط المالك باستخدام tg://user?id=ID
+
+# رابط المالك
 OWNER_URL = f"tg://user?id={OWNER_ID}"
 OWNER_HTML = f'<a href="{OWNER_URL}">{OWNER_NAME}</a>'
-OWNER_ICON = f'<a href="{OWNER_URL}">{⌤}</a>'
-OWNER_ICO = f'<a href="{OWNER_URL}">{⌥}</a>'
+
+# الرموز الخاصة باستخدام Unicode escapes
+OWNER_ICON = f'<a href="{OWNER_URL}">\u2324</a>'  # ⌤
+OWNER_ICO  = f'<a href="{OWNER_URL}">\u2325</a>'  # ⌥
+
+# أداة التطوير
 TOOL_BY = f'{OWNER_HTML} - 🍀'
