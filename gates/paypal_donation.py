0e20b3fd3b6ff61d5f45b1f6b293e966a93b2797
ysvuoh <ali.dhman.201@gmail.com>
2026-02-24 01:48:34 +0200

    Update paypal_donation.py

--- gates/paypal_donation.py
+++ gates/paypal_donation.py
@@ -20,8 +20,8 @@ def paypal1(ccx):#@I_EOR
 	    'user-agent': user,
 	}
 
-	base_url = "https://karimashowandtell.com"
-	donation_page = "https://karimashowandtell.com/donate/"
+	base_url = "https://www.dream-bikes.org"
+	donation_page = "https://www.dream-bikes.org/donate/"
 	amount = "0.50"            	            
 	response = r.get(donation_page, cookies=r.cookies, headers=headers)
 	id_form1 = re.search(r'name="give-form-id-prefix" value="(.*?)"', response.text).group(1)
